e8189d0a71d1ce1756bf092141bb7906b25d7111
Thomas <thomas.luxhoj@secata.com>
2022-11-02 22:11:18 +0100

    Fixed test and added readme

---
+++ readme.md
@@ -0,0 +1,23 @@
+The sparkle in the resource file comes from the zip file, with the following modification to it. A
+main have been added as entry point
+and the following code have been added as written below. This means
+the program accepts a list of integer arguments, and the second last is branch, and last argument is
+step.
+
+```
+int main(int argc, char *argv[]) {
+  uint32_t state[argc - 2];
+
+  for (int i = 1; i < argc - 2; ++i) {
+    state[i - 1] = atoi(argv[i]);
+  }
+  int branch = atoi(argv[argc - 2]);
+  int steps = atoi(argv[argc - 1]);
+  sparkle_opt(state, branch, steps);
+  FILE *f = fopen("test", "wt");
+  for (int i = 0; i < sizeof(state) / sizeof(state[0]) - 1; ++i) {
+    fprintf(f, "%d\n", state[i]);
+  }
+  fclose(f);
+}
+```
--- src/test/java/SparkleTest.java
+++ src/test/java/SparkleTest.java
@@ -2,8 +2,8 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.file.Files;
 import java.util.Arrays;
+import java.util.Scanner;
 import java.util.function.Consumer;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,148 +12,105 @@ import org.junit.jupiter.api.Test;
 /** Test java implementation with C implementation of sparkle. */
 public final class SparkleTest {
 
-  private static final String sparkleCpath = SparkleTest.class.getResource("/sparkle").getPath();
+  private static final String sparkleCpath =
+      SparkleTest.class.getResource("/sparkleDir/sparkle").getPath();
 
-  private static final String[] arguments =
+  private static final String resourceSparkle =
+      System.getProperty("user.dir") + "/src/test/resources/sparkleDir";
+
+  private static final String[] argumentsToOverride =
       new String[] {
         sparkleCpath,
-        "-10",
-        "20",
-        "30",
-        "1",
-        "2",
-        "3",
-        "12",
-        "2",
-        "2",
-        "4",
-        "2",
-        "1",
-        "2",
-        "2",
-        "3",
-        "1",
-        "2",
-        "3",
-        "8",
-        "6",
-        "52",
-        "3",
-        "24",
-        "3",
-        "4",
-        "2",
-        "5",
-        "7",
-        "8",
-        "2",
-        "7",
-        "4",
-        "2",
+        "10",
+        "21238",
+        "57",
+        "38",
+        "75",
+        "92",
+        "71",
+        "234",
+        "96",
+        "72",
+        "94",
+        "72",
+        "9739",
+        "59397",
+        "234",
+        "1231",
         "override",
         "override"
       };
 
   private static final int[] javaStateFinal =
-      new int[] {
-        -10, 20, 30, 1, 2, 3, 12, 2, 2, 4, 2, 1, 2, 2, 3, 1, 2, 3, 8, 6, 52, 3, 24, 3, 4, 2, 5, 7,
-        8, 2, 7, 4, 2
-      };
+      new int[] {10, 21238, 57, 38, 75, 92, 71, 234, 96, 72, 94, 72, 9739, 59397, 234, 1231};
 
   private static int[] javaState;
+  private static String[] arguments;
 
   @BeforeEach
   public void setup() {
     javaState = Arrays.copyOf(javaStateFinal, javaStateFinal.length);
-  }
-
-  @Test
-  void sparkle256TestEmptyStartState() throws IOException, InterruptedException {
-    String[] emptyState =
-        new String[] {
-          sparkleCpath,
-          "0",
-          "0",
-          "0",
-          "0",
-          "0",
-          "0",
-          "0",
-          "0",
-          "0",
-          "0",
-          "0",
-          "0",
-          "0",
-          "0",
-          "0",
-          "0",
-          "0",
-          "0",
-          "0",
-          "0",
-          "0",
-          "0",
-          "0",
-          "0",
-          "0",
-          "0",
-          "0",
-          "0",
-          "0",
-          "0",
-          "0",
-          "0",
-          "0",
-          "0",
-          "0",
-          "0",
-          "4",
-          "10"
-        };
-    callProcess(Sparkle::sparkle256, new int[32], emptyState);
+    arguments = Arrays.copyOf(argumentsToOverride, argumentsToOverride.length);
   }
 
   @Test
   void sparkle256Test() throws IOException, InterruptedException {
+    arguments[arguments.length - 2] = "4";
+    arguments[arguments.length - 1] = "10";
     callProcess(Sparkle::sparkle256);
   }
 
   @Test
   void sparkle256SlimTest() throws IOException, InterruptedException {
+    arguments[arguments.length - 2] = "4";
+    arguments[arguments.length - 1] = "7";
     callProcess(Sparkle::sparkle256Slim);
   }
 
   @Test
   void sparkle384Test() throws IOException, InterruptedException {
+    arguments[arguments.length - 2] = "6";
+    arguments[arguments.length - 1] = "11";
     callProcess(Sparkle::sparkle384);
   }
 
   @Test
   void sparkle384SlimTest() throws IOException, InterruptedException {
+    arguments[arguments.length - 2] = "6";
+    arguments[arguments.length - 1] = "7";
     callProcess(Sparkle::sparkle384Slim);
   }
 
   @Test
   void sparkle512Test() throws IOException, InterruptedException {
+    arguments[arguments.length - 2] = "8";
+    arguments[arguments.length - 1] = "12";
     callProcess(Sparkle::sparkle512);
   }
 
   @Test
   void sparkle512SlimTest() throws IOException, InterruptedException {
+    arguments[arguments.length - 2] = "8";
+    arguments[arguments.length - 1] = "8";
     callProcess(Sparkle::sparkle512Slim);
   }
 
   private static void callProcess(Consumer<int[]> function, int[] state, String[] arguments)
       throws InterruptedException, IOException {
-    Process process = new ProcessBuilder(Arrays.asList(arguments)).start();
+    Process process =
+        new ProcessBuilder(Arrays.asList(arguments)).directory(new File(resourceSparkle)).start();
     int result = process.waitFor();
     if (result != 0) {
       printError(process);
     }
-    File myObj = new File("/mnt/c/Users/tnl/sparkle2/Sparkle/test");
-    byte[] bytes = Files.readAllBytes(myObj.toPath());
-    int[] cState = getStateFromBytes(bytes);
+    File myObj = new File(resourceSparkle + "/test");
+    Scanner scanner = new Scanner(myObj);
+    int[] cState = new int[16];
+    int i = 0;
+    while (scanner.hasNextInt()) {
+      cState[i] = scanner.nextInt();
+      i++;
+    }
     function.accept(state);
     Assertions.assertThat(cState).isEqualTo(state);
   }
@@ -163,17 +120,6 @@ public final class SparkleTest {
     callProcess(function, javaState, arguments);
   }
 
-  private static int[] getStateFromBytes(byte[] bytes) {
-    int intLength = bytes.length / 4;
-    int[] result = new int[intLength];
-    int intIndex = 0;
-    for (int offset = 0; offset < intLength; offset += 4) {
-      result[intIndex] = intFromBytes(bytes, offset);
-      intIndex++;
-    }
-    return result;
-  }
-
   private static void printError(Process process) throws IOException {
     BufferedReader errinput = new BufferedReader(new InputStreamReader(process.getErrorStream()));
     String line = errinput.readLine();
@@ -181,12 +127,5 @@ public final class SparkleTest {
       System.out.println(line);
       line = errinput.readLine();
     }
-  }
-
-  public static int intFromBytes(byte[] bytes, int offset) {
-    return Byte.toUnsignedInt(bytes[0 + offset]) << 24
-        | Byte.toUnsignedInt(bytes[1 + offset]) << 16
-        | Byte.toUnsignedInt(bytes[2 + offset]) << 8
-        | Byte.toUnsignedInt(bytes[3 + offset]);
   }
 }
