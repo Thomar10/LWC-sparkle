bb7e5a0de9454475e61e0b6d1465a8c57246bc6d
Thomas Luxhøj <tlux-8@hotmail.com>
2022-11-24 14:57:17 +0100

    Merge master

--- src/test/java/MaskedSparkleTest.java
+++ src/test/java/MaskedSparkleTest.java
@@ -5,104 +5,105 @@ import java.util.Random;
 
 public class MaskedSparkleTest {
 
-    @RepeatedTest(10)
-    void secondOrderMaskedSparkle256Test(){
-        HigherOrderStateShares stateShares = HigherOrderStateShares.generateStateShares(2);
-        Sparkle.sparkle256(stateShares.state);
-        MaskedSparkle.sparkle256(stateShares.stateShares);
+  @RepeatedTest(10)
+  void secondOrderMaskedSparkle256Test() {
+    HigherOrderStateShares stateShares = HigherOrderStateShares.generateStateShares(2);
+    Sparkle.sparkle256(stateShares.state);
+    MaskedSparkle.sparkle256(stateShares.stateShares);
 
-        int[] recoveredState = HigherOrderStateShares.recoverState(stateShares.stateShares);
+    int[] recoveredState = HigherOrderStateShares.recoverState(stateShares.stateShares);
 
-        Assertions.assertThat(stateShares.state).isEqualTo(recoveredState);
+    Assertions.assertThat(stateShares.state).isEqualTo(recoveredState);
+  }
+
+  @RepeatedTest(10)
+  void thirdOrderMaskedSparkle256Test() {
+    HigherOrderStateShares stateShares = HigherOrderStateShares.generateStateShares(3);
+    Sparkle.sparkle256(stateShares.state);
+    MaskedSparkle.sparkle256(stateShares.stateShares);
+
+    int[] recoveredState = HigherOrderStateShares.recoverState(stateShares.stateShares);
+
+    Assertions.assertThat(stateShares.state).isEqualTo(recoveredState);
+  }
+
+  @RepeatedTest(10)
+  void fourthOrderMaskedSparkle256Test() {
+    HigherOrderStateShares stateShares = HigherOrderStateShares.generateStateShares(4);
+    Sparkle.sparkle256(stateShares.state);
+    MaskedSparkle.sparkle256(stateShares.stateShares);
+
+    int[] recoveredState = HigherOrderStateShares.recoverState(stateShares.stateShares);
+
+    Assertions.assertThat(stateShares.state).isEqualTo(recoveredState);
+  }
+
+  @RepeatedTest(10)
+  void fifthOrderMaskedSparkle256Test() {
+    HigherOrderStateShares stateShares = HigherOrderStateShares.generateStateShares(5);
+    Sparkle.sparkle256(stateShares.state);
+    MaskedSparkle.sparkle256(stateShares.stateShares);
+
+    int[] recoveredState = HigherOrderStateShares.recoverState(stateShares.stateShares);
+
+    Assertions.assertThat(stateShares.state).isEqualTo(recoveredState);
+  }
+
+  @RepeatedTest(10)
+  void higherOrderStateSharesTest() {
+    HigherOrderStateShares stateShares = HigherOrderStateShares.generateStateShares(5);
+
+    Assertions.assertThat(stateShares.state).isNotEqualTo(stateShares.stateShares[0]);
+
+    int[] recoveredState = HigherOrderStateShares.recoverState(stateShares.stateShares);
+
+    Assertions.assertThat(stateShares.state).isEqualTo(recoveredState);
+  }
+
+  record HigherOrderStateShares(int[] state, int[][] stateShares) {
+
+    static HigherOrderStateShares generateStateShares(int order) {
+      int shareLength = Sparkle.maxBranches * 2;
+
+      int[] state = generateRandomShare(shareLength);
+      int[][] stateShares = new int[order][shareLength];
+
+      System.arraycopy(state, 0, stateShares[0], 0, shareLength);
+
+      for (int i = 1; i < order; i++) {
+        stateShares[i] = generateRandomShare(shareLength);
+        maskShare(stateShares[0], stateShares[i]);
+      }
+
+      return new HigherOrderStateShares(state, stateShares);
     }
 
-    @RepeatedTest(10)
-    void thirdOrderMaskedSparkle256Test(){
-        HigherOrderStateShares stateShares = HigherOrderStateShares.generateStateShares(3);
-        Sparkle.sparkle256(stateShares.state);
-        MaskedSparkle.sparkle256(stateShares.stateShares);
-
-        int[] recoveredState = HigherOrderStateShares.recoverState(stateShares.stateShares);
-
-        Assertions.assertThat(stateShares.state).isEqualTo(recoveredState);
+    static void maskShare(int[] share, int[] mask) {
+      for (int i = 0; i < share.length; i++) {
+        share[i] ^= mask[i];
+      }
     }
 
-    @RepeatedTest(10)
-    void fourthOrderMaskedSparkle256Test(){
-        HigherOrderStateShares stateShares = HigherOrderStateShares.generateStateShares(4);
-        Sparkle.sparkle256(stateShares.state);
-        MaskedSparkle.sparkle256(stateShares.stateShares);
+    static int[] generateRandomShare(int length) {
+      Random random = new Random();
 
-        int[] recoveredState = HigherOrderStateShares.recoverState(stateShares.stateShares);
+      int[] share = new int[length];
 
-        Assertions.assertThat(stateShares.state).isEqualTo(recoveredState);
+      for (int i = 0; i < length; i++) {
+        share[i] = random.nextInt(Integer.MAX_VALUE);
+      }
+
+      return share;
     }
 
-    @RepeatedTest(10)
-    void fifthOrderMaskedSparkle256Test(){
-        HigherOrderStateShares stateShares = HigherOrderStateShares.generateStateShares(5);
-        Sparkle.sparkle256(stateShares.state);
-        MaskedSparkle.sparkle256(stateShares.stateShares);
+    static int[] recoverState(int[][] stateShares) {
+      int[] recoveredState = stateShares[0];
 
-        int[] recoveredState = HigherOrderStateShares.recoverState(stateShares.stateShares);
+      for (int i = 1; i < stateShares.length; i++) {
+        maskShare(recoveredState, stateShares[i]);
+      }
 
-        Assertions.assertThat(stateShares.state).isEqualTo(recoveredState);
+      return recoveredState;
     }
-
-    @RepeatedTest(10)
-    void higherOrderStateSharesTest(){
-        HigherOrderStateShares stateShares = HigherOrderStateShares.generateStateShares(5);
-
-        Assertions.assertThat(stateShares.state).isNotEqualTo(stateShares.stateShares[0]);
-
-        int[] recoveredState = HigherOrderStateShares.recoverState(stateShares.stateShares);
-
-        Assertions.assertThat(stateShares.state).isEqualTo(recoveredState);
-    }
-
-    record HigherOrderStateShares(int[] state, int[][] stateShares){
-        static HigherOrderStateShares generateStateShares(int order){
-            int shareLength = Sparkle.maxBranches * 2;
-
-            int[] state = generateRandomShare(shareLength);
-            int[][] stateShares = new int[order][shareLength];
-
-            System.arraycopy(state, 0, stateShares[0], 0, shareLength);
-
-            for(int i = 1; i < order; i++){
-                stateShares[i] = generateRandomShare(shareLength);
-                maskShare(stateShares[0], stateShares[i]);
-            }
-
-            return new HigherOrderStateShares(state, stateShares);
-        }
-
-        static void maskShare(int[] share, int[] mask){
-            for(int i = 0; i < share.length; i++){
-                share[i] ^= mask[i];
-            }
-        }
-
-        static int[] generateRandomShare(int length){
-            Random random = new Random();
-
-            int[] share = new int[length];
-
-            for(int i = 0; i < length; i++){
-                share[i] = random.nextInt(Integer.MAX_VALUE);
-            }
-
-            return share;
-        }
-
-        static int[] recoverState(int[][] stateShares){
-            int[] recoveredState = stateShares[0];
-
-            for(int i = 1; i < stateShares.length; i++) {
-                maskShare(recoveredState, stateShares[i]);
-            }
-
-            return recoveredState;
-        }
-    }
+  }
 }
